de7c2caa587ac5cd5ce41b142a93eee5d1cf0f2d
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-06-01 14:59:17 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
 
 from sqlalchemy_sandbox import Student
 
 engine = create_engine('sqlite:///students.db')
+# Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb
+    ipdb.set_trace()
